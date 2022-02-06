fc76750896fbd247fe9c39904b70d7cbbf4e728d
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2022-02-06 14:24:59 +0700

    can find seiyuu but wanna fix a searching method

--- src/component/Anime.jsx
+++ src/component/Anime.jsx
@@ -6,8 +6,14 @@ const Anime = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+
   const [value, setValue] = useState("");
   const [titleAnime, setTitleAnime] = useState("");
+
+  const [idChara, setIdChara] = useState("");
+  const [errorSeiyuu, setErrorSeiyuu] = useState(null);
+  const [itemsSeiyuu, setItemsSeiyuu] = useState([]);
+  const [isLoadedSeiyuu, setIsLoadedSeiyuu] = useState(false);
 
   // Note: the empty deps array [] means
   // this useEffect will run once
@@ -29,6 +35,10 @@ const Anime = () => {
         console.log(response.data.data);
         setIsLoaded(true);
         setItems(response.data.data);
+        setError(null);
+
+        // Nampung id chara
+        setIdChara(response.data.data[0].mal_id);
       })
       .catch(function (error) {
         // handle error
@@ -37,6 +47,24 @@ const Anime = () => {
         console.log(error);
       });
   }, [titleAnime]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.jikan.moe/v4/characters/${idChara}/voices`)
+      .then(function (response) {
+        // handle success
+        // console.log(response);
+        console.log(response.data.data);
+        setIsLoadedSeiyuu(true);
+        setItemsSeiyuu(response.data.data);
+      })
+      .catch(function (error) {
+        // handle error
+        setIsLoadedSeiyuu(true);
+        setErrorSeiyuu(error);
+        // console.log(error);
+      });
+  }, [idChara]);
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -47,21 +75,72 @@ const Anime = () => {
     console.log(items);
     event.preventDefault();
   };
+
+  const clickChara = (value) => {
+    setTitleAnime(value);
+  };
+
   if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
     return (
       <div className=" min-h-screen bg-neutral-800 flex flex-col justify-center items-center">
-        <h1 className="mb-10 text-5xl text-transparent font-title font-bold bg-clip-text bg-gradient-to-r from-sky-400 to-blue-400">
-          Anime Gan
+        <h1 className="mb-10 text-4xl sm:text-5xl text-transparent font-title font-bold bg-clip-text bg-gradient-to-r from-sky-400 to-blue-400">
+          Find The Seiyuu
         </h1>
         <form onSubmit={handleSubmit}>
           <input
             type="search"
             className="form-input px-4 py-3 rounded-full "
-            placeholder="ex. Death Note"
+            placeholder="ex. Kasumi Toyama"
+            onChange={handleChange}
+            value={value}
+          />
+          <button
+            type="submit"
+            className="mx-2 px-5 py-3 bg-indigo-700 rounded-full text-white hover:bg-indigo-900 text-sm"
+          >
+            Search
+          </button>
+        </form>
+        <div className="text-md font-sans text-blue-300 mt-5 text-center ">
+          Error: {titleAnime} can't found
+        </div>
+      </div>
+    );
+  } else if (!isLoaded) {
+    return (
+      <div className=" min-h-screen bg-neutral-800 flex flex-col justify-center items-center">
+        <h1 className="mb-10 text-4xl sm:text-5xl text-transparent font-title font-bold bg-clip-text bg-gradient-to-r from-sky-400 to-blue-400">
+          Find The Seiyuu
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            className="form-input px-4 py-3 rounded-full "
+            placeholder="ex. Kasumi Toyama"
+            onChange={handleChange}
+            value={value}
+          />
+          <button
+            type="submit"
+            className="mx-2 px-5 py-3 bg-indigo-700 rounded-full text-white hover:bg-indigo-900 text-sm"
+          >
+            Search
+          </button>
+        </form>
+        <div>Loading...</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className=" min-h-screen bg-neutral-800 flex flex-col justify-center items-center">
+        <h1 className="mb-10 text-4xl sm:text-5xl text-transparent font-title font-bold bg-clip-text bg-gradient-to-r from-sky-400 to-blue-400">
+          Find The Seiyuu
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            className="form-input px-4 py-3 rounded-full "
+            placeholder="ex. Kasumi Toyama"
             onChange={handleChange}
             value={value}
           />
@@ -81,6 +160,16 @@ const Anime = () => {
               >
                 <a href={item.url} target="_blank">
                   {item.name}
+                </a>
+              </li>
+            ))}
+            {itemsSeiyuu.map((item) => (
+              <li
+                key={item.person.name}
+                className="bg-red-600 text-white py-2 px-4 my-2 rounded-xl"
+              >
+                <a href={item.person.url} target="_blank">
+                  {item.person.name}
                 </a>
               </li>
             ))}
